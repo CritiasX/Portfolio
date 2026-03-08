ae1c3782a0c71cf80748bdd5bc0f717dbed7b153
James <tjamesroi@gmail.com>
2026-03-08 15:57:44 +0800

    Fix output directory and force PHP errors

--- api/index.php
+++ api/index.php
@@ -1,3 +1,8 @@
 <?php
-// Forward Vercel requests to Laravel's normal index.php
+// Force PHP to show us any hidden fatal errors
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+// Forward Vercel requests to the Laravel index.php
 require __DIR__ . '/../public/index.php';
